b2d0ac973a38c5d48f8d46f198f9757791d7a370
spaceCowboy677 <bworrell2022@outlook.com>
2025-03-05 22:07:29 -0700

    Updating linear algebra routines. Code will not build.

--- src/lu.c
+++ src/lu.c
@@ -1,101 +1,115 @@
+#include "../lib/types/types.h"
 #include "../lib/utils.h"
 #include <math.h>
 
 #define TINY 1.0e-20;
 
-void ludcmp(float **a, int n, int *indx, float *d) {
-  // Given an n x n matrix a[1..n][1..n], this routine replaces it by the LU
+void ludcmp(tensor *a, uint *indx, double *d) {
+  // Given an n x n tensor a[0..n-1][0..n-1], this routine replaces it by the LU
   // decomposition of a rowwise permutation of itself. a and n are input. a is
-  // output; indx[1..n] is an output vector which records the row permutation
+  // output; indx[0..n-1] is an output vector which records the row permutation
   // affected by the partial pivoting; d is output as +/- 1 depending on whether
   // the number of row interchanges was even or odd, respectively. This routine
   // is used in combination with lubksb to solve linear equations or invert a
   // matrix.
-  int imax;
-  float big, dum, sum, temp;
-  float *vv;
+  //
+  // check if matrix is square
+  if (a->rows != a->cols) {
+    error("Non-square matrix in routine LUDCMP");
+  }
 
-  vv = vector(1, n);
+  uint imax;
+  uint n = a->rows;
+  double big, dum, sum, temp;
+  vector vv = vecalloc(n);
+
   *d = 1.0;
-  for (int i = 1; i <= n; i++) {
+  for (uint i = 1; i <= n; i++) {
     big = 0.0;
-    for (int j = 1; j <= n; j++) {
-      if ((temp = fabs(a[i][j])) > big) {
+    for (uint j = 1; j <= n; j++) {
+      if ((temp = fabs(a->data[i][j])) > big) {
         big = temp;
       }
     }
     if (big == 0.0) {
       error("Singular matrix in routine LUDCMP");
     }
-    vv[i] = 1.0 / big;
+    vv.data[i] = 1.0 / big;
   }
   // implement Crout's method
-  for (int j = 1; j <= n; j++) {
-    for (int i = 1; i < j; i++) {
-      sum = a[i][j];
-      for (int k = 1; k < i; k++) {
-        sum -= a[i][k] * a[k][j];
+  for (uint j = 1; j <= n; j++) {
+    for (uint i = 1; i < j; i++) {
+      sum = a->data[i][j];
+      for (uint k = 1; k < i; k++) {
+        sum -= a->data[i][k] * a->data[k][j];
       }
-      a[i][j] = sum;
+      a->data[i][j] = sum;
     }
     big = 0.0; // initialize search for largest pivot element
-    for (int i = j; i < n; i++) {
-      sum = a[i][j];
-      for (int k = 1; k < j; k++) {
-        sum -= a[i][k] * a[k][j];
+    for (uint i = j; i < n; i++) {
+      sum = a->data[i][j];
+      for (uint k = 1; k < j; k++) {
+        sum -= a->data[i][k] * a->data[k][j];
       }
-      a[i][j] = sum;
-      if ((dum = vv[i] * fabs(sum)) >= big) {
+      a->data[i][j] = sum;
+      if ((dum = vv.data[i] * fabs(sum)) >= big) {
         big = dum;
         imax = i;
       }
     }
     if (j != imax) {
-      for (int k = 1; k <= n; k++) {
-        dum = a[imax][k];
-        a[imax][k] = a[j][k];
-        a[j][k] = dum;
+      for (uint k = 1; k <= n; k++) {
+        dum = a->data[imax][k];
+        a->data[imax][k] = a->data[j][k];
+        a->data[j][k] = dum;
       }
       *d = -(*d);
-      vv[imax] = vv[j];
+      vv.data[imax] = vv.data[j];
     }
     indx[j] = imax;
-    if (a[j][j] == 0.0) {
-      a[j][j] = TINY;
+    if (a->data[j][j] == 0.0) {
+      a->data[j][j] = TINY;
     }
     if (j != n) {
-      dum = 1.0 / (a[j][j]);
-      for (int i = j + 1; i <= n; i++) {
-        a[i][j] *= dum;
+      dum = 1.0 / (a->data[j][j]);
+      for (uint i = j + 1; i <= n; i++) {
+        a->data[i][j] *= dum;
       }
     }
   }
-  free_vector(vv, 1, n);
+  vfree(vv);
 }
 
-void lubksb(float **a, int n, int *indx, float b[]) {
-  int ii = 0, ip;
+void lubksb(tensor *a, uint *indx, double b[]) {
+  // Solves the set of n linear equations Ax=b. A is input as the LU
+  // decomposition and indx is the permutation vector determined by LUDCMP.
+  // check if matrix is square
+  if (a->rows != a->cols) {
+    error("Non-square matrix in routine LUDCMP");
+  }
+  uint ii = 0, ip;
+  uint n = a->rows;
   float sum;
 
-  for (int i = 1; i <= n; i++) {
+  for (uint i = 0; i < n; i++) {
     ip = indx[i];
     sum = b[ip];
     b[ip] = b[i];
     if (ii) {
-      for (int j = ii; j <= i - 1; j++) {
-        sum -= a[i][j] * b[j];
+      for (uint j = ii; j <= i - 1; j++) {
+        sum -= a->data[i][j] * b[j];
       }
     } else if (sum) {
       ii = i;
     }
     b[i] = sum;
   }
-  for (int i = n; i >= 1; i--) {
+  for (int i = n - 1; i >= 0; i--) {
     sum = b[i];
     for (int j = i + 1; j <= n; j++) {
-      sum -= a[i][j] * b[j];
+      sum -= a->data[i][j] * b[j];
     }
-    b[i] = sum / (a[i][i]);
+    b[i] = sum / (a->data[i][i]);
   }
 }
 
